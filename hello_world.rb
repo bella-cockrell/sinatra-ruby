71863ecc71592f1168fbd9ab23224827d34efbec
bella-cockrell <arabella.cockrell@gmail.com>
2020-09-17 19:39:54 +0100

    Creating day_of_the_week function

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,10 @@
 require "sinatra"
+require "date"
+
+def day_of_the_week(time)
+    Date::DAYNAMES[time.wday]
+end
 
 get "/" do
-    DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    day = DAYNAMES[Time.now.wday]
-    "Hello everyone! Happy #{day}"
+    "Hello everyone! Happy #{day_of_the_week(Time.now)}"
 end
